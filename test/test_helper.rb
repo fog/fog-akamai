b9c7474845041ebe46056d36b362e900fbb7cef0
Alexandru Calinoiu <calinoiu.alexandru@agilefreaks.com>
2016-02-09 16:14:01 +0200

    [#1] enable code climate minitest test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'minitest/autorun'
 require 'fog/akamai'
+require "codeclimate-test-reporter"
+
+CodeClimate::TestReporter.start
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |rb| require(rb) }
